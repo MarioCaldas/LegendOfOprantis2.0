79a130048b1047ccb4bb9887293dc7e91518a873
MarioCaldas <caldas_96@live.com.pt>
2017-03-15 09:57:23 +0000

    Versao1.0

---
+++ Assets/_Scripts/Movement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+
+    private Animator animation;
+    public float speed = 1f;
+
+
+	void Start ()
+    {
+        animation = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		if(Input.GetAxis("Vertical") > 0)
+        {
+            Vector3 velocity = transform.forward * speed;
+            animation.SetBool("isWalking", true);
+        }
+
+        else
+        {
+            animation.SetBool("isWalking", false);
+        }
+	}
+}
